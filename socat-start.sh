9f0e2136118e730be3dcf9957a4ee92f50528808
Peter Bryzgalov <peterbryz@yahoo.com>
2015-10-09 10:59:53 +0900

    Report PID in socat-start

---
+++ socat-start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Start socat TCP proxy to docker socket on specific port. Default is 4243.
+
+port=4243
+if [ -n "$1" ]; then
+	port=$1
+fi
+
+echo "Start TCP proxy to docker socat runs on port $port"
+socat TCP-LISTEN:$port,fork,reuseaddr UNIX-CONNECT:/var/run/docker.sock &
+echo "Socat runs with PID $!"
