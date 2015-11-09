41e71b563b2d19d3ab08d897affd4c50d48bbdfa
Peter Bryzgalov <peterbryz@yahoo.com>
2015-11-09 22:54:10 +0900

    Modified generate-docker-key to read filename or use

--- generate-docker-key.sh
+++ generate-docker-key.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-
-ssh-keygen -t rsa -N "" -f docker_rsa
+if [[ -z "$1" ]]; then
+    echo -n "Enter key file name:"
+    read filename
+else
+    filename="$1"
+fi
+echo -n "Create key ${filename}? [y/n]"
+read -n 1 start
+if [[ $start == "y" ]]; then
+    ssh-keygen -t rsa -N "" -f $filename
+fi
